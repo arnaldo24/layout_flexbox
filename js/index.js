ce7cdaae8db0e059840c816b2c7993f09b41f967
arnaldo24 <arnaldo24900@gmail.com>
2023-03-16 07:24:34 -0500

    Interactividad con js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log("Hola mundo");
+addEventListener("DOMContentLoaded",()=>{
+
+	const article_txt = document.querySelector("main article");
+	const text_inicial = article_txt.textContent;
+	const ul = document.querySelector("nav ul");
+
+	ul.addEventListener("mouseover",(e)=>{
+		//console.log( e.target )
+		//console.log( article_txt.textContent )
+
+		if( e.target.matches( "li" ) ){
+			// console.log( e.target.textContent )
+			article_txt.textContent = e.target.textContent;
+		}
+	});
+
+	ul.addEventListener("mouseout",(e)=>{
+		//console.log( e.target )
+		
+		article_txt.textContent = text_inicial;
+		
+	})
+})
